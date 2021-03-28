193d348fd1320a09f6127e83d0f4793677390fd8
Görkem Aran <GrkemAran@hotmail.com>
2021-03-28 19:41:27 +0300

    Day 17 - Refacoring 1

---
+++ src/app/models/rental.ts
@@ -0,0 +1,12 @@
+export interface Rental{
+    rentalId:number;
+    carId:number;
+    customerId:number;
+    carName:string;
+    userName:string;
+    brandName:string;
+    description:string;
+    dailyPrice:number;
+    rentDate:Date;
+    returnDate:Date;
+}
